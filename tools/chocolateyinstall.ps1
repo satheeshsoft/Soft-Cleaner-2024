1bfc570a596e95c0dd06034f4bc9f5daff5ac3c1
Sathish <42292676+satheeshsoft@users.noreply.github.com>
2024-03-20 19:46:25 +0530

    Add files via upload

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.Command.Define1000)"
+$fileLocation = Join-Path- $toolsDir 'SCSetup.exe'
+$packageName = 'Soft Cleaner'
+$packageArgs = @{
+  }
+Install-ChocolateyInstallPackage @packageArgs
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Soft Cleaner'
+  fileType      = 'EXE' 
+  silentArgs   = '/NORESTART /SP-'  
+}
+$uninstalled = $true
+[array]$key = Get-UninstallRegistryKey -SoftwareName-$packageArg['softwareName']
+}
